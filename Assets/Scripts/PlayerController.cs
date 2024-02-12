d32ce4be0876bc81a755499820967374fdb33c08
Барышев Степан Сергеевич <Baryshev.S.S@corp.kp11.ru>
2024-02-12 13:10:19 +0300

    0.1.1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private Transform _rotaionPoint;
 
-    public float moveSpeed = 1f;
-    public float rotationSpeed = 3f;
+    private Rigidbody _rigidbody;
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
 
     private void FixedUpdate()
     {
+        transform.eulerAngles = new Vector3 (0f, _rotaionPoint.eulerAngles.y, 0f);
 
+        float moveHorizontal = Input.GetAxis("Horizontal");
 
-        float currentSpeed = moveSpeed * Input.GetAxis("Vertical");
+        float moveVertical = Input.GetAxis("Vertical");
 
-        _characterController.Move(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        transform.Translate (movement * moveSpeed * Time.fixedDeltaTime);
+
+        if (Input.GetAxis("Jump") > 0)
+        {
+            _rigidbody.AddForce(Vector3.up * jumpForce);
+        }
     }
 }
